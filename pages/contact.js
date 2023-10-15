6a60127ed7a127e9b49cfc8ae3da75617156f9fa
Wjplatformer <fongwenjun@hotmail.com>
2023-10-15 18:11:42 +0800

    major revamps

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyXRSieYv8mdYaGk3_Fti1pztBT8C_3_WBuCdx9xtl9ipKv_YQYIDnQUwhjX2MSO6hk/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
